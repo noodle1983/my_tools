44d783430868af0871316a7032940a9e87b093f5
LuDong <noodle1983@126.com>
2020-05-11 20:15:24 +0800

    Update sync_p4_with_git.cmd

--- sync_p4_with_git.cmd
+++ sync_p4_with_git.cmd
@@ -4,6 +4,8 @@ REM p4 set P4USER={user}
 REM p4 set P4CHARSET=utf8
 REM p4 set P4CLIENT={workspace}
 REM git p4 clone {//path} {local_dir}
+git config apply.ignorewhitespace change
+git config core.longpaths true
 git reset HEAD --hard
 echo {pass}|p4 login
 git p4 sync
