8dd0bff0181bb814e0ce9a9e57a4257ce1cf8c75
LuDong <noodle1983@126.com>
2020-05-11 19:21:25 +0800

    Update sync_p4_with_git.cmd

--- sync_p4_with_git.cmd
+++ sync_p4_with_git.cmd
@@ -2,6 +2,7 @@ REM p4 set P4PORT={host:1666}
 REM p4 set P4PASSWD={pass}
 REM p4 set P4USER={user}
 REM p4 set P4CHARSET=utf8
+REM p4 set P4CLIENT={workspace}
 REM git p4 clone {//path} {local_dir}
 git reset HEAD --hard
 echo {pass}|p4 login
